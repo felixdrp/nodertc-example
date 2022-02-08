42c1a71c7b0e4857615f6b14ec40d6e64ab33326
felixdrp <felixdrp@gmail.com>
2022-02-08 17:47:17 +0000

    Added client and certificates

---
+++ fixtures/client.js
@@ -0,0 +1,95 @@
+'use strict';
+
+/* eslint-env browser */
+
+const pcconfig = {
+  iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+};
+
+const pc = new RTCPeerConnection(pcconfig);
+
+pc.addEventListener('icecandidate', async ({ candidate }) => {
+  console.log('got candidate', candidate);
+
+  if (candidate) {
+    return;
+  }
+
+  const offer = pc.localDescription;
+  console.log('[pc] send offer\n', offer.sdp);
+  const answer = await sendOffer(offer);
+  console.log('[pc] got answer\n', answer.sdp);
+  await pc.setRemoteDescription(answer);
+});
+
+pc.addEventListener('negotiationneeded', async () => {
+  console.log('[dc] negotiationneeded');
+
+  const offer = await pc.createOffer();
+  await pc.setLocalDescription(offer);
+});
+
+pc.addEventListener('datachannel', ({ channel }) => {
+  console.log('got channel', channel);
+
+  channel.addEventListener('open', () => {
+    console.log('[dc] channel is ready', channel);
+
+    channel.send(`Hello, world!`);
+  });
+
+  channel.addEventListener('close', () => {
+    console.log('[dc] channel is closed');
+  });
+
+  channel.addEventListener('message', ({ data }) => {
+    console.log('got message: %s', data.toString());
+  });
+});
+
+const channel = pc.createDataChannel('console');
+
+channel.addEventListener('open', () => {
+  console.log('[dc] channel is ready', channel);
+});
+
+channel.addEventListener('close', () => {
+  console.log('[dc] channel is closed');
+});
+
+channel.addEventListener('message', async (event) => {
+  const  { data } = event;
+  const dataType = data.constructor.name;
+  let msg;
+  switch (dataType) {
+    case 'Blob':
+      msg = await data.text();
+      break;
+    case 'ArrayBuffer':
+      msg = String.fromCharCode.apply(null, new Uint8Array(data))
+      break;
+    default:
+      msg = data.toString();
+  }
+  console.log('[dc] type %s, got message: %s', dataType, msg);
+
+  channel.send(`Hello, NodeRTC!`);
+});
+
+/**
+ * Send offer.
+ * @param {{ sdp: string, type: string }} offer
+ * @returns {{ sdp: string, type: string }}
+ */
+async function sendOffer(offer) {
+  const res = await fetch('/offer', {
+    method: 'post',
+    body: JSON.stringify(offer),
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return res.json();
+}
